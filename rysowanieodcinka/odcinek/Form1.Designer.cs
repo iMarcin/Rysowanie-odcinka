b8538183b884e0ca14c192439faab1e6e074fc2f
iMarcin <64076685+iMarcin@users.noreply.github.com>
2020-05-27 20:06:38 +0200

    Add files via upload

---
+++ rysowanieodcinka/odcinek/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace odcinek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "rysuj przyrostowy";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ax";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "ay";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "bx";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(2, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "by";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(43, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 36);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "rysuj srodkowy";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(181, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(43, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(43, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(43, 130);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(181, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(456, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "300";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(162, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(150, 320);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "300";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(520, 359);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ rysowanieodcinka/odcinek/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odcinek
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            pictureBox1.Image = null;
+            Bitmap obraz = new Bitmap(300, 300);
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+            {
+                int ax = Convert.ToInt32(textBox1.Text);
+                int ay = Convert.ToInt32(textBox2.Text);
+                int bx = Convert.ToInt32(textBox3.Text);
+                int by = Convert.ToInt32(textBox4.Text);
+                if(ax>bx)
+                {
+                    int t = ax;
+                    ax = bx;
+                    bx = t;
+                    t = ay;
+                    ay = by;
+                    by = t;
+
+                }
+                double x;
+                double dy, dx, y, m;
+                dy = by - ay;
+                dx = bx - ax;
+                if (bx-ax >= by-ay)
+                {
+                    m = dy / dx;
+                    y = ay;
+                    for (x = ax; x < bx; x++)
+                    {
+                        if (x < 300)
+                        {
+                            obraz.SetPixel(Convert.ToInt32(x), Convert.ToInt32(Math.Round(y + 0.5)), Color.Green);
+                            y += m;
+                            pictureBox1.Image = obraz;
+                        }
+                    }
+                }
+                else
+                {
+                    m = dx / dy;
+                    x = ax;
+                    for (y = ay; y < by; y++)
+                    {
+                        if (y < 300)
+                        {
+                            obraz.SetPixel(Convert.ToInt32(Math.Round(x + 0.5)), Convert.ToInt32(y), Color.Green);
+                            x += m;
+                            pictureBox1.Image = obraz;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            Bitmap obraz = new Bitmap(300, 300);
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+            {
+                int ax = Convert.ToInt32(textBox1.Text);
+                int ay = Convert.ToInt32(textBox2.Text);
+                int bx = Convert.ToInt32(textBox3.Text);
+                int by = Convert.ToInt32(textBox4.Text);
+                if (ax > bx)
+                {
+                    int t = ax;
+                    ax = bx;
+                    bx = t;
+                    t = ay;
+                    ay = by;
+                    by = t;
+
+                }
+
+                if (by > ay)
+                {
+                    int dx, dy, incre, incrne, d, x, y;
+                    dx = bx - ax;
+                    dy = by - ay;
+                    x = ax;
+                    y = ay;
+                    obraz.SetPixel(x, y, Color.Blue);
+                    if (bx - ax >= by - ay)
+                    {
+                        d = dy * 2 - dx;
+                        incre = dy * 2;
+                        incrne = (dy - dx) * 2;
+                        while (x < bx)
+                        {
+                            if (d <= 0)
+                            {
+                                d += incre;
+                                x++;
+                            }
+                            else
+                            {
+                                d += incrne;
+                                x++;
+                                y++;
+                            }
+                            if (x < 300)
+                                obraz.SetPixel(x, y, Color.Blue);
+                        }
+                    }
+                    else
+                    {
+                        d = dx * 2 - dy;
+                        incre = dx * 2;
+                        incrne = (dx - dy) * 2;
+                        while (y < by)
+                        {
+                            if (d <= 0)
+                            {
+                                d += incre;
+                                y++;
+                            }
+                            else
+                            {
+                                d += incrne;
+                                y++;
+                                x++;
+                            }
+                            if (y < 300)
+                                obraz.SetPixel(x, y, Color.Blue);
+                        }
+
+                    }
+                }else
+                {
+                    int dx, dy, incre, incrne, d, x, y;
+                    dx = bx - ax;
+                    dy = ay - by;
+                    x = bx;
+                    y = by;
+                    obraz.SetPixel(x, y, Color.Blue);
+                    if (bx - ax >= ay - by)
+                    {
+
+                        d = dy * 2 - dx;
+                        incre = dy * 2;
+                        incrne = (dy - dx) * 2;
+                        while (x > ax)
+                        {
+                            if (d <= 0)
+                            {
+                                d += incre;
+                                x--;
+                            }
+                            else
+                            {
+                                d += incrne;
+                                x--;
+                                y++;
+                            }
+                            if (x > 0)
+                                obraz.SetPixel(x, y, Color.Blue);
+                        }
+                    }
+                    else
+                    {
+                        d = dx * 2 - dy;
+                        incre = dx * 2;
+                        incrne = (dx - dy) * 2;
+                        while (y > ay)
+                        {
+                            if (d <= 0)
+                            {
+                                d += incre;
+                                y--;
+                            }
+                            else
+                            {
+                                d += incrne;
+                                y--;
+                                x++;
+                            }
+                            if (y > 0)
+                                obraz.SetPixel(x, y, Color.Blue);
+                        }
+
+                    }
+
+                }
+                pictureBox1.Image = obraz;
+            }
+        }
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
